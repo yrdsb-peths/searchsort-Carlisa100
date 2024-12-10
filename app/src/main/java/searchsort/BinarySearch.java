a94533881b0fea786d4adb781023b228de9fc7c5
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-12-10 01:21:02 -0500

    Binary and Linear Search Code

--- app/src/main/java/searchsort/BinarySearch.java
+++ app/src/main/java/searchsort/BinarySearch.java
@@ -5,9 +5,22 @@ public class BinarySearch extends Search {
      * Find the index of the target element in the sorted array arr using binary
      * search. Return -1 if target is not found in arr.
      */
+
     @Override
     public int find(int[] arr, int target) {
-        return 0;
+        int lo = 0;
+        int hi = arr.length - 1;
+        while (lo <= hi) {
+           int mid = (lo + hi) / 2;
+           if (target < arr[mid])
+                hi = mid - 1;
+         else if (target > arr[mid])
+             lo = mid + 1;    
+            else
+                return mid;
+     }
+       return -1;
+
     }
 
 }
